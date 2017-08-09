89162851c1b43ebfd58054f9f9afb1f3df34a896
Zach <earthospreyhd@gmail.com>
2017-08-09 16:53:25 +1000

    improved window movement

--- movewindow.cpp
+++ movewindow.cpp
@@ -23,6 +23,18 @@ int xoff;
 
 int yoff;
 
+int rat1;
+
+int rat2;
+
+int lesseradd;
+
+int greateradd;
+
+int inc1;
+
+int inc2;
+
 int main()
 {
 
@@ -32,46 +44,120 @@ int main()
 
 	MoveWindow(conhandle, x, y, 300, 400, TRUE);
 
+	int destx;
+
+	int desty;
+
+	double grad1;
+
+	double grad2;
+
+	int gradient;
+
+	bool xislarger = FALSE;
+
+	//void bumpwin(int up, int bot) {
+
+
+
+	//}
+
 	while (true) {
 
 		conhandle = GetForegroundWindow();
 
 		GetCursorPos(&curpos);
 
-		xoff = curpos.x - x;
+		xoff = curpos.x - x - 150;
 
-		yoff = curpos.y - y;
+		yoff = curpos.y - y - 200;
 
-		if (xoff < 450 && xoff > 300 && yoff > 0 && yoff < 550) {
-			//is in the right rectangle
-			x = curpos.x - 450;
+		if (xoff < 250 && xoff > -250 && yoff < 250 && yoff > -250) {
+
+			//cout << xoff << " ";
+
+			//cout << yoff << endl;
+
+			destx = xoff;
+
+			desty = yoff;
+
+			if (abs(destx) > abs(desty)) {
+
+				grad1 = destx;
+
+				grad2 = desty;
+
+				greateradd = x;
+
+				lesseradd = y;
+
+				inc1 = (abs(xoff) == xoff) ? -1 : 1;
+
+				inc2 = (abs(yoff) == yoff) ? -1 : 1;
+
+				xislarger = TRUE;
+
+			}
+
+			else {
+
+				grad1 = desty;
+
+				grad2 = destx;
+
+				greateradd = y;
+
+				lesseradd = x;
+
+				inc1 = (abs(yoff) == yoff) ? -1 : 1;
+
+				inc2 = (abs(xoff) == xoff) ? -1 : 1;
+			}
+
+			int greater = (int)grad1;
+
+			int lesser = (int)grad2;
+
+			gradient = (int)grad1 / grad2;
+
+			for (int z = 0; z < 300; z++) {
+
+				if (z % gradient == 0) {
+
+					lesseradd = lesseradd + inc2;
+
+				}
+
+				greateradd = greateradd + inc1;
+
+				if (xislarger) {
+
+					MoveWindow(conhandle, greateradd, lesseradd, 300, 400, FALSE);
+
+					cout << "x is " << greateradd << " y is " << lesseradd << " " << abs(greater / lesser + 1) << endl;
+
+					x = greateradd;
+
+					y = lesseradd;
+
+				}
+
+				else {
+
+					MoveWindow(conhandle, lesseradd, greateradd, 300, 400, FALSE);
+
+					cout << "y is " << greateradd << " x is " << lesseradd << " " << abs(greater / lesser) << endl;
+
+					y = greateradd;
+
+					x = lesseradd;
+				}
+
+			}
+
 
 		}
-
-		if (xoff < 0 && xoff > -150 && yoff > 0 && yoff < 550) {
-			//is in the left rectangle
-			x = curpos.x + 150;
-
-		}
-
-		if (yoff > 400 && yoff < 550 && xoff > 0 && xoff < 300) {
-			// is in the bottom rectangle
-			y = curpos.y - 550;
-
-		}
-
-		if (yoff < 0 && yoff > -150 && xoff > 0 && xoff < 300) {
-			// is in the top rectangle
-			y = curpos.y + 150;
-
-		}
-
-		MoveWindow(conhandle, x, y, 300, 400, FALSE);
-
-		cout << curpos.x << " ";
-
-		cout << curpos.y << endl;
-
 	}
 
 	return 0;
